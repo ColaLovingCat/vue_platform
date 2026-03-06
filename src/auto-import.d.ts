e1df613cccd3fd41f630b498dc3716deabd1e92d
CIO2SZH <Lingyang.Cao@cn.bosch.com>
2026-03-06 17:35:21 +0800

    update

--- src/auto-import.d.ts
+++ src/auto-import.d.ts
@@ -24,6 +24,7 @@ declare module 'vue' {
     AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
     AModal: typeof import('ant-design-vue/es')['Modal']
     ARadio: typeof import('ant-design-vue/es')['Radio']
+    ARadioButton: typeof import('ant-design-vue/es')['RadioButton']
     ARadioGroup: typeof import('ant-design-vue/es')['RadioGroup']
     ARangePicker: typeof import('ant-design-vue/es')['RangePicker']
     ASelect: typeof import('ant-design-vue/es')['Select']
